951e13c9ec1d8f9872c9042ccbce5a72a8049b13
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-04-17 16:48:56 +0530

    Update IconSlider.jsx

--- LearnFlow/src/components/IconSlider.jsx
+++ LearnFlow/src/components/IconSlider.jsx
@@ -33,14 +33,14 @@ const styles = {
   icon: {
     fontSize: "30px",
     color: "white",
-    background: "#3498db",
+    background: "white",
     borderRadius: "50%",
     width: "60px",
     height: "60px",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+    /* boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)", */
   },
 };
 
