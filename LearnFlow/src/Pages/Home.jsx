07f63cbf04099df59dc5bae0771cb0fe7ab1c159
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-05-05 20:54:10 +0530

    Update Home.jsx

--- LearnFlow/src/Pages/Home.jsx
+++ LearnFlow/src/Pages/Home.jsx
@@ -3,7 +3,6 @@ import image from '../assets/bgggg.png';
 import CardContainer from '../components/CardContainer';
 import IconSlider from '../components/IconSlider';
 
-
 const Home = ({ onGetStarted }) => {
   return (
     <div
@@ -12,19 +11,40 @@ const Home = ({ onGetStarted }) => {
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        width: "100vw",
-        height: "100vh",
-        position: "relative", // Changed from fixed to relative
+        width: "100%",
+        minHeight: "100vh",
+        position: "relative",
         zIndex: 0,
       }}
     >
-      <div style={{ color: "white", textAlign: "left", paddingTop: "15%", paddingLeft: "10%" }}>
-        <h1 className='text-container' style={{ fontSize: "3rem", fontWeight: "bold" }}>
+      <div
+        style={{
+          color: "white",
+          textAlign: "left",
+          paddingTop: "15%",
+          paddingLeft: "10%",
+          paddingRight: "10%",
+        }}
+      >
+        <h1
+          className="text-container"
+          style={{
+            fontSize: "3rem",
+            fontWeight: "bold",
+            lineHeight: "1.2",
+          }}
+        >
           Start Your Learning Path
           <br />
-          With Our Services kjdshkfjhsdf
+          With Our Services
         </h1>
-        <p style={{ fontSize: "1.2rem", margin: "20px 0" }}>
+        <p
+          style={{
+            fontSize: "1.2rem",
+            margin: "20px 0",
+            lineHeight: "1.5",
+          }}
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
           Sed do eiusmod tempor incididunt ut labore et dolore.
         </p>
@@ -47,7 +67,6 @@ const Home = ({ onGetStarted }) => {
       </div>
       <CardContainer />
       <IconSlider />
-    
     </div>
   );
 };
